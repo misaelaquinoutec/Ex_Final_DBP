e22caf49e6b8169a34048a724b5dd2d97de8b86a
misaelaquinoutec <misael.aquino@utec.edu.pe>
2026-07-11 08:57:39 -0500

    falta mi dashboard

--- src/componets/Dashboard.tsx
+++ src/componets/Dashboard.tsx
@@ -1,1 +1,15 @@
 import { useEffect, useState } from "react";
+import { apiservises } from "../services/api";
+import type { PageProductsResponse } from "../types/types";
+
+export const Dashboard: React.FC = () => {
+    const [data, setData] = useState<PageProductsResponse | null>(null);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        setLoading(true);
+        apiservises.getProducts(0, 8)
+            .then(res => setData(res))
+            .catch(err => console.error(err))
+            .finally(() => setLoading(false));
+    }       , []);
